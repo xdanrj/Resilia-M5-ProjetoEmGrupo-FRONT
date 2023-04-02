a48e54b0ea2609c1099e52592aa660d9035b47d8
xdanrj <116215888+xdanrj@users.noreply.github.com>
2023-04-02 15:09:36 -0300

    Produtos centralizados

--- src/pages/Produtos/CardProdutos.jsx
+++ src/pages/Produtos/CardProdutos.jsx
@@ -4,16 +4,28 @@ import "./CardProdutos.css"
 import { deleteProdutos } from "../../api/ApiProdutos";
 
 const CardProdutos = ({id, nome, cor, preco, img}) => {
-        console.log(id)
-    
+
 return(
         <div className='container'>
+        <div className='divTextos'>
         <img src={img}/>
-        
+        </div>
+
+        <div className='divTextos'>
         <p>{nome}</p>
+        </div>
+
+        <div className='divTextos'>
         <p>Cor: {cor}</p>
+        </div>
+
+        <div className='divTextos'>
         <p>Preco: {preco}</p>
-        <button onClick={(e) => deleteProdutos(id)}>Deletar</button>
+        </div>
+        
+
+        <div className='botaoDeletar'><button onClick={(e) => deleteProdutos(id)}>Deletar</button></div>
+
         </div>
 )}
 
